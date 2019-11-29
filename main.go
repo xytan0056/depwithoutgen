682cac05edff458e19b56f32e3c6e94db5040c67
Xiaoyang Tan <tanx@uber.com>
2019-11-29 15:05:58 -0800

    add files

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt" 
+
+func some() {
+	fmt.Print("I'm depwithgen") 
+}
---
+++ main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"testing"
+	c "github.com/xytan0056/depwithoutgen/.gen/client"
+)
+
+func TestSomeTest(t *testing.T) {
+	t.Logf("testing on %s", c.Client("007"))
+}
